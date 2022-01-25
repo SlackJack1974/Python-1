655145012a659cf1d5e073d6d341175af4726b50
SlackJack1974 <Karsten.goldschmidt@gmx.net>
2022-01-25 22:08:36 +0100

    Erstes Programm

---
+++ Reiskörner.py
@@ -0,0 +1,10 @@
+summe = 0
+for feld in range(64):
+    reiskorn = 2**feld
+    summe = summe + reiskorn
+    print("Feld {}. = {:>30,} Reiskörner und damit insgesamat {:>30,} Reiskörner" \
+          .format(feld+1,reiskorn,summe))
+gewicht = summe * 0.02 / 1000 / 1000
+print ()
+print("Wenn ein Reiskorn 0,02 Gramm wiegt, wiegen die gesamten")
+print("Reiskörner {:18,.0f} Tonnen".format(gewicht))
